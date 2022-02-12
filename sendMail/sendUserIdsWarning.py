68a5cd51c3a578ba5a27eaed8b9c8f552aabd825
Meet Rajput <rajputmeet10@outlook.com>
2022-02-12 20:34:32 +0530

    py for sending warning mails

---
+++ sendMail/sendUserIdsWarning.py
@@ -0,0 +1,16 @@
+import smtplib
+  
+s = smtplib.SMTP('smtp.gmail.com', 587)
+  
+s.starttls()
+senderMail=input("Enter your e-mail id: ")
+senderPass=input("Enter your password: ")
+s.login(senderMail, senderPass)
+  
+message = "Dear customer, your account credentials have been leaked. Kindly change your login details."
+
+usernames=open("\\dataFiles\\userNames.txt","r")
+for mails in usernames:
+    s.sendmail(senderMail, mails, message)
+
+s.quit()
